5db9c23f24b51db4605da04298a7a70e8175ecbc
Yasas077 <rmygamitha@gmail.com>
2024-09-29 19:42:37 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class Q4 {
+
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("My First Frame");
+            frame.setSize(300, 200);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocation(100, 50); // 100 pixels from the left, 50 pixels from the top
+            frame.setVisible(true);
+        }
+    }
+
